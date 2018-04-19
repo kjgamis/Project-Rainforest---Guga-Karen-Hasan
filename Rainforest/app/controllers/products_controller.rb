81a6b1c661f854856393055ebc3a80765f016c7e
Hasan Naseem <hn92@live.com>
2018-04-19 12:02:04 -0400

    create models, controllers and routes for Products

---
+++ Rainforest/app/controllers/products_controller.rb
@@ -0,0 +1,32 @@
+class ProductsController < ApplicationController
+
+  def index
+
+    @products = Product.all
+
+  end
+
+  def new
+
+  end
+
+  def edit
+
+  end
+
+  def show
+
+  end
+
+  def update
+
+  end
+
+  def create
+
+  end
+
+  def destroy
+
+  end
+end
