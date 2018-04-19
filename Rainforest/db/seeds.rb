454c9a268723b2aa4fa3d419cdcbc3015e643224
Karen <kjgamis@live.ca>
2018-04-19 13:08:09 -0400

    add products in seed

--- Rainforest/db/seeds.rb
+++ Rainforest/db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Product.create(name: 'Chair', description: 'black wooder', price_in_cents: 900)
+Product.create(name: 'Aeroplane', description: 'Airbus A380', price_in_cents: 3400000)
+Product.create(name: 'Rock', description: 'hard', price_in_cents: 1)
+Product.create(name: 'yoyo', description: 'spinny', price_in_cents: 20)
