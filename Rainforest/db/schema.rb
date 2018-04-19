802ab6091e1ae6a38e9e4d8c14cab06b1b7988d8
Hasan Naseem <hn92@live.com>
2018-04-19 15:14:29 -0400

    created review

--- Rainforest/db/schema.rb
+++ Rainforest/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_04_19_152139) do
+ActiveRecord::Schema.define(version: 2018_04_19_185453) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -18,6 +18,13 @@ ActiveRecord::Schema.define(version: 2018_04_19_152139) do
     t.integer "price_in_cents"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "product_id"
   end
 
 end
